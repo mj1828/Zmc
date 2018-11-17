93afb4c56c2e027a37cd3183048ef8469180c53a
张昌昌 <zhcc@zving.com>
2018-11-18 00:55:40 +0800

    更改代码

--- src/main/java/com/mj/ZmcZasApplication.java
+++ src/main/java/com/mj/ZmcZasApplication.java
@@ -1,12 +1,12 @@
-package com.mj.zas;
+package com.mj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MJExamZasApplication {
+public class ZmcZasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MJExamZasApplication.class, args);
+		SpringApplication.run(ZmcZasApplication.class, args);
 	}
 }
